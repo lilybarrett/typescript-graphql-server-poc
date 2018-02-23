74972185242dfe0192f7df3763f3e644196dac4d
Lily Barrett <lily.barrett@ef.com>
2018-02-23 12:13:06 -0500

    Update resolvers

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -17,10 +17,19 @@ const channels = [
     },
 ];
 
+let nextId = 3;
+
 export const resolvers = {
     Query: {
         channels: () => {
             return channels;
+        },
+    },
+    Mutation: {
+        createChannel: ( root: any, args: any ) => {
+            const newChannel = { id: nextId++, name: args.name };
+            channels.push(newChannel);
+            return newChannel;
         },
     },
 };
