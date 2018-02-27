55c0eab4fe21d63153614d4777ef56bac5e99226
Lily Barrett <lily.barrett@ef.com>
2018-02-27 11:35:37 -0500

    Add full CRUD queries and mutations

--- schema/root/mutations.ts
+++ schema/root/mutations.ts
@@ -2,9 +2,18 @@ import { Channel } from "../../models";
 
 export default {
         Mutation: {
-            createChannel: async ( parent: any, args: any) => {
-                const newChannel = Channel.create({ name: args.name });
+            createChannel: async ( parent, args ) => {
+                const newChannel = await Channel.create({ name: args.name });
                 return newChannel;
+            },
+            updateChannelName: async ( parent, args ) => {
+                const channelToUpdate = await Channel.findById(args.id);
+                channelToUpdate.update( { name: args.newName }, { where: { id: args.id } });
+                return channelToUpdate;
+            },
+            deleteChannel: async ( parent, args ) => {
+                Channel.destroy( { where: { id: args.id } } );
+                return args.id;
             },
     },
 };
